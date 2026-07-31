5b6cc1fe92f45bab08219369b1e46bbd023a31c0
NavnathKakde <ndkakde08@gmail.com>
2026-07-31 06:05:54 +0530

    Assignment 1-Data Types

---
+++ typescript-training/assignment1-dataTypes.ts
@@ -0,0 +1,87 @@
+//1. Temperature of city in degree celsius :25.5
+let temperature:number = 25.5;
+console.log(`1. Temperature of city in degree celsius :${temperature}`);
+
+// 2. Weather a customer has placed an order: True or false
+let placeAnOrder:boolean = true;
+console.log(`2. Weather a customer has placed an order: ${placeAnOrder}`);
+
+// 3. Person's phone number: "123-456-7890"
+let phoneNumber:String = "123-456-7890";
+console.log(`3. Person's phone number: ${phoneNumber}`);
+
+// 4. Amount of money in customer's bank account: 1000.50
+let accountBalance:number = 1000.50
+console.log(`4. Amount of money in customer's bank account: ${accountBalance}`);
+
+// 5. Person's email address: "John-doe@example.com"
+let emailAddress:string = "John-doe@example.com";
+console.log(`5. Person's email address: ${emailAddress}`)
+
+// 6. Coordinate of location (Latitude, longitude) : 37.7749, -122.4194
+let Latitude:number = 37.7749;
+let longitude:number = -122.4194;
+console.log(`6. Coordinate of location (Latitude, longitude) : ${Latitude},${longitude}`);
+
+// 7. Person's marital status: true or false
+let maritalStatus:boolean = true;
+console.log(`7. Person's marital status: ${maritalStatus}`)
+
+// 8. Person's occupations: "Software Engineer";
+let personOccupation:String = "Software Engineer";
+console.log(`8. Person's occupations: ${personOccupation}`)
+
+// 9. Person's favourite colour:"Blue"
+let personFavouriteColour: string = "Blue";
+console.log(`9. Person's favourite colour: ${personFavouriteColour}`)
+
+// 10. Current year: 2023
+let currentYear:number =2023;
+console.log(`10. Current year: ${currentYear}`)
+
+// 11. Number of followers on social media platform: 1,00,000
+let socialMediaFollowers:string = "1,00,000"
+console.log(`11. Number of followers on social media platform: ${socialMediaFollowers}`)
+
+// 12. Rating of movie: 7.5
+let ratingOfMovie:number = 7.5
+console.log(`12. Rating of movie: ${ratingOfMovie}`)
+
+// 13. Persons blood group: 'A'
+let personBloodGroup:string = 'A'
+console.log(`13. Persons blood group: ${personBloodGroup}`)
+
+// 14. Title of a book "To kill a MockheinBird"
+let titleOfBook:string = "To kill a MockheinBird";
+console.log(`14. Title of a book ${titleOfBook}"`)
+
+// 15.Number of employees in a company: 500
+let numberOfEmployeeInCompany:number = 500;
+console.log(`15.Number of employees in a company: ${numberOfEmployeeInCompany}`)
+
+// 16.Time of an event: 2:30 PM
+let timeOfEvent:string ="2.30 PM";
+console.log(`16.Time of an event: ${timeOfEvent}`)
+
+// 17.Name of a country: "United States"
+let nameOfCountry:string = "United States"
+console.log(`17.Name of a country: ${nameOfCountry}`)
+
+// 18.Person's eye color: "Brown"
+let personEyeColour:string ="Brown";
+console.log(`18.Person's eye color: ${personEyeColour}`)
+
+// 19.Person's birthplace: "New York City"
+let personBirthPlace:string = "New York City"
+console.log(`19.Person's birthplace: ${personBirthPlace}`)
+
+// 20. Distance between two cities: 200.5
+let distanceBetweenTwoCities:number = 200.5
+console.log(`20. Distance between two cities: ${distanceBetweenTwoCities}`)
+
+
+
+
+
+
+
